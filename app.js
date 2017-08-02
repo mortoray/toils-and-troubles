f23104e145a6606c01cb753ade8390e2312870e6
edA-qa on MBP <edA-qa@disemia.com>
2017-08-02 20:10:53 +0200

    upgrade to findRouter to replace hack

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
 //this setup is an ugly hack to avoid passing `router` as a dependency everywhere
-exports.router = undefined
-exports.setRouter = function( nrouter ) {
-	exports.router = nrouter
-}
+
 
